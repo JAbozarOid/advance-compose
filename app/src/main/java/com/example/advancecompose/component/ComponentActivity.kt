bdd8cbfe96dfedf255e817f702862baa1308d8cd
jabozaroid <araghibdoust@hotmail.com>
2023-09-04 16:55:35 +0330

    snackbar

--- app/src/main/java/com/example/advancecompose/component/ComponentActivity.kt
+++ app/src/main/java/com/example/advancecompose/component/ComponentActivity.kt
@@ -1,7 +1,8 @@
 package com.example.advancecompose.component
 
-import android.graphics.drawable.Icon
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,12 +23,20 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,7 +46,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.advancecompose.R
 import com.example.advancecompose.ui.theme.AdvanceComposeTheme
+import kotlinx.coroutines.launch
 
+private const val TAG = "ABOZAR"
 class BoxActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,6 +71,7 @@ class BoxActivity : ComponentActivity() {
                     textButton()
                     outlineButton()
                     iconButton()
+                    displaySnackBar()
                 }
             }
 
@@ -154,12 +166,12 @@ private fun simpleButton() {
     Button(
         onClick = { Toast.makeText(context, "Button 1", Toast.LENGTH_SHORT).show() },
         contentPadding = PaddingValues(16.dp),
-        border = BorderStroke(10.dp,Color.Black),
+        border = BorderStroke(10.dp, Color.Black),
         colors = ButtonDefaults.textButtonColors(
             contentColor = Color.White,
         ),
         shape = CutCornerShape(10.dp)
-        ) {
+    ) {
         Text(text = "Button 1")
     }
 }
@@ -219,5 +231,45 @@ fun iconButton() {
 @Composable
 fun iconButtonPreview() {
     iconButton()
+}
+
+/**
+ * in order to display snack bar we need to have two things
+ * 1- A scaffold state : is a layout which implement the basic material structure
+ * 2- A Coroutine scope
+ */
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "CoroutineCreationDuringComposition")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun displaySnackBar() {
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(snackbarHost = { SnackbarHost (snackbarHostState)}, modifier = Modifier.fillMaxSize()) {
+        Button(onClick = {
+            scope.launch {
+                val snackBarResult = snackbarHostState.showSnackbar(
+                    message = "This is a snackbar message",
+                    actionLabel = "Undo",
+                    duration = SnackbarDuration.Long
+                )
+                when(snackBarResult) {
+                    SnackbarResult.Dismissed -> Log.i(TAG, "dismissed")
+                    SnackbarResult.ActionPerformed -> Log.i(TAG, "action label clicked")
+                }
+            }
+        }) {
+
+            Text(text = "Display Snackbar")
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun snackBarPreview() {
+    displaySnackBar()
 }
 
