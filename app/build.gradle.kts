39715b28cd361208a7d861e131dfc26871ac216a
jabozaroid <araghibdoust@hotmail.com>
2023-09-10 23:34:33 +0330

    core-module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+//    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.example.advancecompose"
-    compileSdk = 33
+    compileSdk = rootProject.ext.get("compileSdkVersion") as Int?
 
     defaultConfig {
         applicationId = "com.example.advancecompose"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = rootProject.ext.get("minSdkVersion") as Int?
+        targetSdk = rootProject.ext.get("targetSdkVersion") as Int?
         versionCode = 1
         versionName = "1.0"
 
@@ -38,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -45,6 +47,23 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    flavorDimensions += "version"
+    productFlavors {
+        create("demo") {
+            dimension = "version"
+            applicationIdSuffix = ".demo"
+            versionNameSuffix = "-demo"
+
+            buildConfigField("String", "BASE_URL", "\"https://fakestoreapi.com/\"")
+        }
+        create("prod") {
+            dimension = "version"
+            applicationIdSuffix = ".full"
+            versionNameSuffix = "-full"
+
+            buildConfigField("String", "BASE_URL", "\"https://fakestoreapi.com/\"")
         }
     }
 }
@@ -62,8 +81,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
