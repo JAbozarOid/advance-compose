917c528491244d0ff8d1e89eb1745cb97c391fc4
jabozaroid <araghibdoust@hotmail.com>
2023-09-13 17:53:18 +0330

    adding dependency injection core module

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.0" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 }
 
 buildscript {
@@ -10,5 +11,13 @@ buildscript {
         set("minSdkVersion", 24)
         set("targetSdkVersion", 33)
         set("compileSdkVersion", 33)
+
+        set("okhttp_version","4.10.0")
+        set("retrofit_version","2.9.0")
+
+        set("moshi","1.13.0")
+
+        set("hilt_version",2.44)
+        set("hilt_navigation_compose","1.0.0")
     }
 }
