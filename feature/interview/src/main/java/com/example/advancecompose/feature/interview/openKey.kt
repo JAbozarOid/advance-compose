e3e637f2b5990505d02630308c0981db6f3de80a
jabozaroid <araghibdoust@hotmail.com>
2024-03-20 10:40:21 +0330

    open

---
+++ feature/interview/src/main/java/com/example/advancecompose/feature/interview/openKey.kt
@@ -0,0 +1,41 @@
+package com.example.advancecompose.feature.interview
+
+// open for class, variable and function
+
+/**
+ * the open for the class means the class is open for extension
+ * in Kotlin all classes are final by default, they can not be inherited
+ * it's opposite of java
+ */
+open class Mentor() {
+
+    open val learner: String = ""
+
+    open fun guide() {
+        println("Everyday You need to be learn new thing")
+    }
+
+    /**
+     * functions in Kotlin are final by default, it means they can not be override
+     */
+    fun selfMotivated() {
+
+    }
+
+
+}
+
+/**
+ * class Experience is final
+ */
+class Experience : Mentor() {
+    override fun guide() {
+        println("some days learn more")
+    }
+}
+
+
+fun main() {
+    val experience = Experience()
+    println(experience.guide())
+}
