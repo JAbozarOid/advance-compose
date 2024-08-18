787ce8ebf218d1152f8f0c65a4510355746f81a1
jabozaroid <a.raghibdoust@sadadpsp.ir>
2024-08-18 15:07:22 +0330

    code challenges

--- feature/interview/src/main/java/com/example/advancecompose/feature/interview/algoritm/AddUpTo.kt
+++ feature/interview/src/main/java/com/example/advancecompose/feature/interview/algoritm/AddUpTo.kt
@@ -14,13 +14,38 @@ class AddUpTo {
         return (1..n).sum()
     }
 
+    /**
+     * fold is a method that accumulates all elements into a single variable (called an "accumulator") using a defined operation.
+     * For instance, let's say that our collection contains the numbers from 1 to 4, our initial accumulator value is 0, and our operation is addition.
+     */
     fun idiomaticSolution2(n: Int): Int {
-        return (0..n).fold(0){accumulated,current -> accumulated + current}
+        return (0..n).fold(0) { accumulated, current -> accumulated + current }
     }
 
-    fun recursiveSolution(n : Int) : Int {
+    fun recursiveSolution(n: Int): Int {
         if (n == 1) return 1
 
-        return n + recursiveSolution(n-1)
+        return n + recursiveSolution(n - 1)
     }
+
+    // time complexity : o(1)
+    fun mathematicalFormulaWithO1(n: Int): Int {
+        return n * (n + 1) / 2
+    }
+
+    // time complexity : o(n)
+    // iterative solution
+    fun iterativeSolutionWithON(n: Int): Int {
+        var total = 0
+        repeat((0..n).count()) { total += 1 }
+        return total
+    }
+
+    fun iterativeSolution(n: Int): Int {
+        var total = 0
+        repeat(n + 1) { total += it }
+        return total
+    }
+
+
 }
