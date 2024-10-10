85b3e3ad3d088beba84aaed3e9a43cc4afc37304
jabozaroid <araghibdoust@hotmail.com>
2024-10-10 19:18:22 +0330

    inline class or value class

---
+++ feature/interview/src/main/java/com/example/advancecompose/feature/interview/kotlinQ/inline/inlineClass.kt
@@ -0,0 +1,16 @@
+package com.example.advancecompose.feature.interview.kotlinQ.inline
+
+/**
+ * when use inline classes, it means the body of class will be inline, it means the body will be copy and paste
+ */
+
+class CallerService {
+
+    fun call(phoneNumber : String) {
+
+    }
+}
+
+fun getNumber() : String {
+    return "9891211111111"
+}
